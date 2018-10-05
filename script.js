be0ed74e54381fcaa1a237eda41ebc99ea0323d4
AkashJawale <43819424+AkashJawale@users.noreply.github.com>
2018-10-05 23:01:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,31 @@
 	var app=angular.module("githubViewer",[]);
 
 	var MainController = function($scope, $http){
+		
 	var onUserComplete = function(response){
+		
 		$scope.user=response.data;
+		$http.get($scope.user.repos_url)
+		.then(onRepos, onError);
 	};
+	var onRepos=function(response){
+	$scope.repos =response.data;
+	};
+	
 	var onError=function(reason){
-		$scope.error="Could not fetch the user";
+		$scope.error="Could not fetch the data.";
 	};
-	$http.get("https://api.github.com/user/robconery")
+
+	$scope.search=function(){
+		console.log('search : '+$scope.username);
+		$http.get("https://api.github.com/users/" + $scope.username)
 	.then(onUserComplete, onError);
-	$scope.message="Hello, Angular!";
+	
 	};
-	app.controller("MainController",MainController);
+	
+	$scope.username="angular"
+	$scope.message="GitHub Viewer";
+	$scope.repoSortOrder="stargazers_count";
+	};
+	app.controller("MainController",["$scope", "$http",MainController]);
 }());
